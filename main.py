6f4712c4bc9516270fb92b946898fdf3cebd0ba9
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-05-26 07:41:21 -0400

    conexion a bd

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, url_for
+import psycopg2
+import psycopg2.extras
 
 app = Flask(__name__)
+app.secret_key="cairocoders-ednalan"
+
+DB_HOST = "localhost"
+DB_NAME = "sampledb"
+DB_USER = "postgres"
+DB_PASS = "admin"
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    s = "SELECT * FROM students"
+    cur.execute(s) # Execute the SQL
+    list_users = cur.fetchall()
+    return render_template('index.html', list_users = list_users)
+
+
+@app.route('/add_student', methods=['POST'])
+def add_student():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST':
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email']
+        cur.execute("INSERT INTO students (fname, lname, email) VALUES (%s, %s, %s)", (fname, lname, email))
+        conn.commit()
+        flash('Student Added successfully')
+        return redirect(url_for('Index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
