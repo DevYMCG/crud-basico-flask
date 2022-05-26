eb1b8570b750194ff61d005fa236923bbb3de5fa
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-05-26 09:48:51 -0400

    crud basico

--- main.py
+++ main.py
@@ -34,5 +34,45 @@ def add_student():
         return redirect(url_for('Index'))
 
 
+@app.route('/edit/<id>', methods=['POST', 'GET'])
+def get_employee(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cur.execute('SELECT * FROM students WHERE id=%s', (id))
+    data = cur.fetchall()
+    cur.close()
+    print(data[0])
+    return render_template('edit.html', student = data[0])
+
+@app.route('/update/<id>', methods=['POST'])
+def update_student(id):
+    if request.method == 'POST':
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email'] 
+
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("""
+            UPDATE students
+            SET fname = %s,
+                lname = %s,
+                email = %s
+            WHERE id = %s
+        """, (fname, lname, email, id))
+        flash('Student Updated Successfully')
+        conn.commit()
+        return redirect(url_for('Index'))
+
+
+@app.route('/delete/<string:id>', methods=['POST', 'GET'])
+def delete_student(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cur.execute('DELETE FROM students WHERE id= {0}'.format(id))
+    conn.commit()
+    flash('Student Removed Successfully')
+    return redirect(url_for('Index'))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
